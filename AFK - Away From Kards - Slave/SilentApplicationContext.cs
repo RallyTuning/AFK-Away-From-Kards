c9bb241d487ffde34b48536732e83400fca305dd
Gianluigi <50233545+RallyTuning@users.noreply.github.com>
2026-04-30 23:55:30 +0200

    Aggiungere i file di progetto.

---
+++ AFK - Away From Kards - Slave/SilentApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFK_Away_From_Kards_Slave
+{
+    public class SilentApplicationContext : ApplicationContext
+    {
+        private readonly SteamIdlerCore? SilentIdlerCore;
+
+        public SilentApplicationContext(IdlerConfig config)
+        {
+            try
+            {
+                SilentIdlerCore = new SteamIdlerCore(config);
+
+                // Close the application when idling is finished
+                SilentIdlerCore.OnFinished += (s, e) =>
+                {
+                    SilentIdlerCore.Dispose();
+                    Application.Exit();
+                };
+
+                SilentIdlerCore.Start();
+            }
+            catch
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
